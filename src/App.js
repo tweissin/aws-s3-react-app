369d13e65c2d1b6ae721ee4bf05bfc755c66f4e7
Tom Weissinger <tweissin@gmail.com>
2023-06-04 13:52:56 -0400

    make api call

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { useFetch } from "react-async"
 import { useEffect } from 'react';
 
 const APIEndPoint = 'https://t42v7vbhd2.execute-api.us-east-1.amazonaws.com/Staging/'
 // const APIEndPoint = 'https://httpbin.org/post'
 // const APIEndPoint = 'http://localhost:8080/cors'
 
-// const json = fetch(APIEndPoint, {
-//   method: "POST",
-//   headers: { 
-//     'Accept': "application/json",
-//     'Content-Type':'application/json' 
-//   },
-//   mode: 'no-cors',
-//   body: '{}'//JSON.stringify({"id": 1, "name": "Joe Cool"})
-// })
-//   .then(response => response)
-//   .then(data => console.log("foo",data))
-//   .catch((error,res) => console.log("err",error))
-
 function App() {
   const makeAPICall = async () => {
     try {
-      // const response = await fetch('http://localhost:8080/', {mode:'cors'});
       const response = await fetch(APIEndPoint, {
         mode:'cors',
         method: "POST"
@@ -36,7 +21,7 @@ function App() {
     }
   }
   useEffect(() => {
-    // makeAPICall();
+    makeAPICall();
   }, [])
 
   return (
@@ -54,30 +39,9 @@ function App() {
         >
           Learn React
         </a>
-        {APIEndPoint.startsWith('http') &&
-            <APIResult />
-          }
       </header>
     </div>
   );
-}
-
-const APIResult = () => {
-  console.log("API re")
-  const { data, error } = useFetch(APIEndPoint, {
-    method: "POST",
-    headers: { accept: "application/json" },
-    body: {"id": 1, "name": "Joe Cool"}
-  })
-  if (error) {
-    console.log("dd")
-    return <p>aaa {error.message}</p>
-  }
-  if (data) {
-    console.log("dd")
-    return <p>bbb {data.message}</p>
-  }
-  return "nothing"
 }
 
 export default App;
